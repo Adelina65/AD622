51b8ad41d51c05efdf6f34e91698e30a233d6f79
Adelina <adelina.stashkevich.97@mail.ru>
2026-09-06 10:51:07 +0300

    second commit

--- homework_11.py
+++ homework_11.py
@@ -92,32 +92,35 @@ import sqlite3
 #     cur.execute('''CREATE TABLE IF NOT EXISTS lessons (
 #         id INTEGER PRIMARY KEY AUTOINCREMENT,
 #         lesson_title TEXT
-#     )''')
+# #     )''')
+#
+#
+# import sqlite3
+#
+#
+# def read_ava(n):
+#     try:
+#         with open(f"avatars/{n}.png", "rb") as f:
+#             return f.read()
+#     except IOError as e:
+#         print(e)
+#         return False
+#
+#
+# with sqlite3.connect('cars.db') as con:
+#     con.row_factory = sqlite3.Row
+#     cur = con.cursor()
+#
+#     cur.executescript('''CREATE TABLE IF NOT EXISTS users (
+#           name TEXT,
+#           ava BLOB,
+#           score INTEGER
+#          )''')
+#
+#     img = read_ava(1)
+#     if img:
+#         binary = sqlite3.Binary(img)
+#         cur.execute("INSERT INTO users VALUES('Федор', ?, 1000)", (binary,))
 
 
-import sqlite3
-
-
-def read_ava(n):
-    try:
-        with open(f"avatars/{n}.png", "rb") as f:
-            return f.read()
-    except IOError as e:
-        print(e)
-        return False
-
-
-with sqlite3.connect('cars.db') as con:
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-
-    cur.executescript('''CREATE TABLE IF NOT EXISTS users (
-          name TEXT,
-          ava BLOB,
-          score INTEGER 
-         )''')
-
-    img = read_ava(1)
-    if img:
-        binary = sqlite3.Binary(img)
-        cur.execute("INSERT INTO users VALUES('Федор', ?, 1000)", (binary,))
+print('Изменения после коммита')
